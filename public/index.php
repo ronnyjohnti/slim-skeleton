b0175281130ee1b1ea32e81fed4f4f3eb296190d
Ronny John <me@ronnyjohnti.dev>
2024-07-13 18:16:31 -0300

    chore: Renaming directory config/ to app/

--- public/index.php
+++ public/index.php
@@ -5,9 +5,11 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$container = require __DIR__ . '/../config/container.php';
+$container = require __DIR__ . '/../app/container.php';
 $app = AppFactory::createFromContainer($container);
 
-require __DIR__ . '/../config/routes.php';
+require __DIR__ . '/../app/routes.php';
+
+$app->addErrorMiddleware(true, true, true);
 
 $app->run();
